0fec82744d6d6442404c7cf9018cac674bf3cc97
= <=>
2021-02-01 00:22:28 +0000

    API project setup

---
+++ api/src/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import { json, urlencoded } from 'body-parser';
+import cors from 'cors';
+
+const PORT = process.env.PORT || 3000;
+
+export const app = express();
+
+app.use(helmet());
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+export const start = () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`REST API listening on port ${PORT}`);
+        });
+    } catch (err) {
+        console.error(err);
+    }
+};
